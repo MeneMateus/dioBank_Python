3cd824c1ebb9826c8a80c594da98f041396b3927
MeneMateus <mateusboxxx@gmail.com>
2024-05-19 17:05:53 -0300

    feito sistema de extrato

--- banco.py
+++ banco.py
@@ -1,28 +1,32 @@
-TEXTO_MENU = "\nBANCO SUPERTOP PYTHON \n\n\ndigite 1 - Para Sacar\ndigite 2 - Para Depositar\ndigite 3 - para ver o saldo\ndigite 0 - Para sair"
+TEXTO_MENU = "\nBANCO SUPERTOP PYTHON \n\n\ndigite 1 - Para Sacar\ndigite 2 - Para Depositar\ndigite 3 - Para Extrato\ndigite 0 - Para sair\n"
 opcao = input(TEXTO_MENU)
 opcao = int(opcao)
 saldo = 0
+limite_diario = 0
 def sair():
     global opcao
     opcao = 0
 
 def verSaldo():
-    print("Seu saldo é de: ", saldo)
+    print(f"\nSeu extrato é de: {saldo:.2f}")
 def depositar():
     global saldo
-    valor = int(input("Digite o valor a ser depositado: "))
+    valor = int(input("Digite o valor a ser depositado: \n"))
     saldo = saldo + valor
 
 def sacar():
+    global limite_diario
     global saldo
-    valor = int(input("Digite o valor a ser sacado: "))
+
+    valor = int(input("Digite o valor a ser sacado: \n"))
     if valor > saldo:
-        print("Saldo insuficiente\n")
+        print("\nSaldo insuficiente\n")
         return
-    if valor > 500:
-        print("Saque não permitido\n")
+    if valor > 500 or limite_diario == 3:
+        print("\nSaque não permitido\n")
         return
     saldo = saldo - valor
+    limite_diario += 1
     
 
 while not opcao == 0:
